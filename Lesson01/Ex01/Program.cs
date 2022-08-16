13fe228a34c42a47b16a5148ceb7139caad9fa73
Savelev Sergey <Select666@mail.ru>
2022-08-16 15:31:44 +0300

    Задача ex01 решена

---
+++ Lesson01/Ex01/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу, которая на вход принимает число и выдает его квадрат.
+// Например: 
+// 2 -> 4, 
+// -3 -> 9 и тд.
+
+System.Console.WriteLine("Это моя программа ССВ");
+int value = 10;
+int result = value * value;
+Console.WriteLine($"result:{result}");
+
+
