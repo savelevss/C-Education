b1d41ecee26a9d7d04ee80647ebaf69c5d6fea29
Savelev Sergey <Select666@mail.ru>
2022-08-17 15:47:04 +0300

    Добавлена восьмая задача

---
+++ Homework01/ex08/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Введите число:   ");
+string number = Console.ReadLine();
+
+int lastcount = Convert.ToInt32(number);
+int num = 0;
+
+Console.WriteLine($"Чётные числа от 1 до {lastcount}:");
+while (num < lastcount)
+{
+    num++;
+    if (num % 2 == 0) Console.Write($"{num} ");
+}
+Console.WriteLine(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
