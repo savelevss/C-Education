f0554fe85e6cb9b4d92a399bdaec43e4778d6975
Savelev Sergey <Select666@mail.ru>
2022-08-17 14:21:31 +0300

    Добавлена четвертая задача

--- Homework01/ex04/Program.cs
+++ Homework01/ex04/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите  первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите  второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите  третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b>max) max=b;
+if (c>max) max=c;
+
+Console.Write("max ");
+Console.WriteLine(max);
