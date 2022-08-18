33797b5421b68c3d6d98d0b323a3ab5ab260dcf5
Savelev Sergey <Select666@mail.ru>
2022-08-18 12:25:55 +0300

    Добавлена задача про диапазон чисел от -N до N

---
+++ Lesson01/ex04/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу, которая на вход принимает число N, и выводит числа в диапазоне от -N до N
+
+
+Console.Write("Введите число  ");
+int x = int.Parse(Console.ReadLine());
+int count = 0;
+while (count<x+x+1)
+{
+Console.Write(-x+count);
+count++;
+}
+
+
+
+
