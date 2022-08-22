64194877d5aafb2f79e25beb8a20f42eeacf5252
Savelev Sergey <Select666@mail.ru>
2022-08-22 14:59:58 +0300

    Второй семинар и домашняя работа

---
+++ Homework02/ex012/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+//что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine ("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>100)
+{
+    string str = number.ToString();
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Homework02/ex015/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+Console.WriteLine ("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int [] week = {1,2,3,4,5};
+int size = week.Length;
+if (number>size)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Рабочий");
+}
+
+
