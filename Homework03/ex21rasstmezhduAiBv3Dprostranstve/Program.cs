edc8e6723544a9f86dd09960658f0fbd1f949862
Savelev Sergey <Select666@mail.ru>
2022-08-27 17:35:21 +0300

    домашнее задание к третьему семинару

--- Homework03/ex21rasstmezhduAiBv3Dprostranstve/Program.cs
+++ Homework03/ex21rasstmezhduAiBv3Dprostranstve/Program.cs
@@ -2,8 +2,8 @@
 // Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
-// double GetNum6()
-// {
+double GetNum6()
+{
    
     Console.Write("Введите координату X для точки A ");
     int xa = Convert.ToInt32 (Console.ReadLine());
@@ -18,16 +18,16 @@
     Console.Write("Введите координату Z для точки B ");
     int zb = Convert.ToInt32 (Console.ReadLine());
     
-    double AB = Math.Sqrt((xb-xa)^2+(yb-ya)^2+(zb-za)^2);
-    Console.WriteLine(AB);
-    // Console.Write("Расстояние между A и B в 3D пространстве равно ");
-    // return AB;
-    // }
-// try
-// {       
-//        Console.WriteLine(GetNum6());
-// }
-// catch 
-// {
-//     Console.WriteLine("Нужно вводить только целые числа ");
-//}
+    double AB = Math.Sqrt((xb-xa)*(xb-xa)+(yb-ya)*(yb-ya)+(zb-za)*(zb-za));
+    // Console.WriteLine(AB);
+    Console.Write("Расстояние между A и B в 3D пространстве равно ");
+    return AB;
+}
+try
+{       
+Console.WriteLine(GetNum6());
+}
+catch 
+{
+Console.WriteLine("Нужно вводить только целые числа ");
+}
---
+++ Homework03/ex23kubychisel/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void CubeTableN()
+{
+    Console.Write("Введите целое число ");
+    int N = Convert.ToInt32(Console.ReadLine());
+    if (N>0)
+    {
+        for (int i = 1; i<(N+1); i++)
+        {
+            Console.Write ($"{i*i*i}, ");
+        }
+    }
+    else
+    {
+        Console.Write("Введите положительные числа");
+    }
+}
+try 
+{
+    CubeTableN();
+}
+catch
+{
+    Console.Write("Введите только положительные числа");
+}
+
