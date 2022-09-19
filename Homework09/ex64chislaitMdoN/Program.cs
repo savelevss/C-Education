b772f821fc9153857844bc9aaaa88d8637b9cefc
Savelev Sergey <Select666@mail.ru>
2022-09-19 14:41:40 +0300

    Дз по Семинар 9

---
+++ Homework09/ex64chislaitMdoN/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+string SequenceRec(int M, int N)
+{
+    if (N==M-1) return String.Empty;
+    return SequenceRec(M, N-1)+ " " + Convert.ToString(N);
+}
+try
+{
+    Console.Write("Введите число M:   ");
+    string M = Console.ReadLine();
+    int count1 = Convert.ToInt32(M);
+    Console.Write("Введите число N:   ");
+    string N = Console.ReadLine();
+    int count2 = Convert.ToInt32(N);
+    Console.Write($"Все натуральные числа в промежутке от {M} до {N} -> ");
+    Console.WriteLine(SequenceRec(count1,count2));
+}
+catch
+{
+    Console.WriteLine("Введите натуральное число!");
+}
---
+++ Homework09/ex66summaotMdoN/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+    
+Console.Write("Введите число M:   ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:   ");
+int n = Convert.ToInt32(Console.ReadLine());
+    
+Console.WriteLine($"Сумма элементов от {m} до {n} = {SumRec(m, n)}");
+
+int SumRec(int m, int n)
+{
+    if (m == n)  return n;
+    return n + SumRec(m, n - 1);
+}
+
